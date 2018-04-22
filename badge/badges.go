4c3d326d20b0ce710438c3f3f258262ac10c11dd
Joe Groocock <me@frebib.net>
2018-04-22 13:37:18 +0100

    Split sendBadge functionality for re-use

--- badge/badges.go
+++ badge/badges.go
@@ -2,10 +2,25 @@ package badge
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spritsail/go-badge"
 	"github.com/urfave/cli"
+	"net/http"
+	"time"
 )
 
 func BuildRoutes(router *gin.RouterGroup, c *cli.Context) (err error) {
 	router.GET("/lastbuild/*repo", lastBuildBadge)
 	return
+}
+
+func sendBadge(req *gin.Context, left string, right string, color string) {
+	badge, err := badge.RenderDef(left, right, badge.Color(color))
+	if err != nil {
+		req.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	req.Header("Content-Type", "image/svg+xml")
+	req.Header("Cache-Control", "no-cache, no-store, must-revalidate")
+	req.Header("Expires", time.Now().UTC().Format(time.RFC1123Z))
+	req.String(http.StatusOK, badge)
 }
--- badge/lastbuild.go
+++ badge/lastbuild.go
@@ -3,11 +3,9 @@ package badge
 import (
 	"github.com/dustin/go-humanize"
 	"github.com/gin-gonic/gin"
-	"github.com/spritsail/go-badge"
 	"github.com/spritsail/image-info/microbadger"
 	"net/http"
 	"strings"
-	"time"
 )
 
 func lastBuildBadge(req *gin.Context) {
@@ -24,6 +22,7 @@ func lastBuildBadge(req *gin.Context) {
 
 	switch status {
 	case http.StatusOK:
+		// If a tag is specified, use the tag Created time instead
 		parts := strings.Split(repo, ":")
 		if len(parts) > 1 {
 			ver := api.GetTag(parts[1], &info)
@@ -38,13 +37,5 @@ func lastBuildBadge(req *gin.Context) {
 		color = "red"
 	}
 
-	badge, err := badge.RenderDef(left, right, badge.Color(color))
-	if err != nil {
-		req.AbortWithStatus(http.StatusInternalServerError)
-		return
-	}
-	req.Header("Content-Type", "image/svg+xml")
-	req.Header("Cache-Control", "no-cache, no-store, must-revalidate")
-	req.Header("Expires", time.Now().UTC().Format(time.RFC1123Z))
-	req.String(http.StatusOK, badge)
+	sendBadge(req, left, right, color)
 }
