f8c4a8fe21f606c420ea5020ec74fe89cf3a60d2
Joe Groocock <me@frebib.net>
2018-05-02 21:10:22 +0100

    Remove useless spritsail/numtext dependency

--- badge/dockerhub.go
+++ badge/dockerhub.go
@@ -1,18 +1,18 @@
 package badge
 
 import (
+	"github.com/dustin/go-humanize"
 	"github.com/gin-gonic/gin"
 	mb "github.com/spritsail/image-info/microbadger"
-	"github.com/spritsail/numtext"
 )
 
 func pullsBadge(req *gin.Context) {
 	repoInfo(req, "blue", "docker pulls", func(info *mb.Image, repo string, color string, left string) (string, string, string) {
-		return color, left, numtext.ToText(info.PullCount)
+		return color, left, humanize.SIWithDigits(float64(info.PullCount), 1, "")
 	})
 }
 func starsBadge(req *gin.Context) {
 	repoInfo(req, "blue", "docker stars", func(info *mb.Image, repo string, color string, left string) (string, string, string) {
-		return color, left, numtext.ToText(info.StarCount)
+		return color, left, humanize.SIWithDigits(float64(info.StarCount), 1, "")
 	})
 }
